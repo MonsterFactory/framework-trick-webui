fbdbe8a43e050865a809fe5681be6b2676af7a39
Daniel.Leung <littleAsteroid@163.com>
2018-06-03 21:40:18 +0800

    修改git路劲

--- Prop/Common/DebuggingTools/IncludeModule.js
+++ Prop/Common/DebuggingTools/IncludeModule.js
@@ -45,7 +45,7 @@ var Module_Include = function(){
 
 function moduleLoad(param, pathUrl, callBackFunction){
     console.log("Trick framework v1.0");
-    console.log("gitHub:https://github.com/MonsterFactory/framework-trick-webui/tree/master/Christmas");
+    console.log("gitHub:https://github.com/MonsterFactory/framework-trick-webui/");
     console.log("               ---Designed by Daniel.Leung.");
     console.log("");
     moduleConfig['controlJs'] = param;
